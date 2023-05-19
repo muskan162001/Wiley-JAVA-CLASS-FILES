aef6e68a2fb593258ffd6c87f15c100d14b115c3
muskan162001 <muskanjain162001@gmail.com>
2023-05-19 11:54:06 +0530

    Store Procedure File

---
+++ MYSQL_CODES/store_procedures.sql
@@ -0,0 +1,31 @@
+-- Active: 1683962974216@@127.0.0.1@3306@wileyclassic
+DELIMITER $$
+
+CREATE PROCEDURE GetCustomers()
+BEGIN
+SELECT
+    customerName,
+    city,
+    state,
+    postalCode,
+    country
+FROM
+    customers;
+ORDER BY
+    customerName;
+END$$
+DELIMITER;
+
+
+--For All Products
+DELIMITER $$
+
+CREATE PROCEDURE GetALLProducts()
+BEGIN
+SELECT
+    *
+FROM
+    products;
+END$$
+
+DELIMITER;

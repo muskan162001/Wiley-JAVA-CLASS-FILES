0a93087cf31024d5d25c469322eff56bc4fe8e6a
muskan162001 <muskanjain162001@gmail.com>
2023-05-26 12:56:35 +0530

    Projects

---
+++ bookpub/src/main/java/com/wiley/bookpub/view/App.java
@@ -0,0 +1,10 @@
+package com.wiley.bookpub.view;
+import com.wiley.bookpub.view.MainView;
+
+public class App {
+
+    public static void main( String[] args )
+    {
+        MainView.choice();
+    }
+}

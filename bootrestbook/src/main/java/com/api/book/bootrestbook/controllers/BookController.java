e6fb4c0c25f639937bc3604baff79e238823c21f
muskan162001 <muskanjain162001@gmail.com>
2023-06-07 11:27:06 +0530

    Update on Spring

---
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -0,0 +1,49 @@
+package com.api.book.bootrestbook.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.book.bootrestbook.entities.Book;
+import com.api.book.bootrestbook.services.BookService;
+
+@RestController
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+
+    //@RequestMapping(value = "/books", method = RequestMethod.GET)
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return this.bookService.getAllBooks();
+    }
+
+    @GetMapping("/books/{id}")
+    public Book getBook(@PathVariable("id") int id) {
+        return bookService.getBookById(id);
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book) {
+        Book b = this.bookService.addBook(book);
+        return b;
+    }
+    
+    //delete the book
+    @DeleteMapping("/books/{bookId}")
+    public void deleteBook(@PathVariable("bookId") int bookId){
+        this.bookService.deleteBook(bookId);
+
+    }
+}
---
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/services/BookService.java
@@ -0,0 +1,48 @@
+package com.api.book.bootrestbook.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.api.book.bootrestbook.entities.Book;
+
+//@Component for autowired
+@Component
+public class BookService {
+    private static List<Book> list = new ArrayList<>();
+
+    static{
+        list.add(new Book(4875, "Java foundation", "XYZ"));
+        list.add(new Book(12, "Java Head", "ABC"));
+        list.add(new Book(13, "Java Break", "LMN"));
+    }
+
+    //get all books
+    public List<Book> getAllBooks() {
+        return list;
+    }
+
+    //get single book by id
+    public Book getBookById(int id) {
+        Book book = null;
+        book = list.stream().filter(e->e.getId()==id).findFirst().get();
+        return book;
+    }
+
+    //adding the book
+    public Book addBook(Book b) {
+        list.add(b);
+        return b;
+    }
+
+    //delete book
+    public void deleteBook(int bid) {
+        list = list.stream().filter(book->book.getId()!= bid
+        ).collect(Collectors.toList());
+    }
+
+}
